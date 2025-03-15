ba233515bbf6e43e5f9f49e50e5620e5034c38a4
hagee <hagee.ktr@outlook.com>
2025-03-15 10:49:32 +0300

    Style the image section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ramens = [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" },
     { id: 4, name: "Naruto Ramen", restaurant: "Ramen-ya", image: "naruto.jpg" }
  ];
